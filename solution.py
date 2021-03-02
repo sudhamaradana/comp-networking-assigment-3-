7ed94a507d4ccd8b78ffe6c88ac6fc2cb2524077
Sudha Maradana <sm9598@nyu.edu>
2021-03-01 20:46:23 -0600

    version 9
    version 9

--- solution.py
+++ solution.py
@@ -7,70 +7,70 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
 
 # server = "localhost"
-server = '127.0.0.1'
-port1 = 1025
-emailFrom = "sm9598@nyu.edu"
-emailTo = "sm9598@nyu.edu"
+    server = '127.0.0.1'
+    port1 = 1025
+    emailFrom = "sm9598@nyu.edu"
+    emailTo = "sm9598@nyu.edu"
 
 # Connect to the local host (an EECS server, where this code should be executed)
-mailserver = (server, port1)
+    mailserver = (server, port1)
 
 # Create socket called clientSocket and establish a TCP connection with nailserver
 # Fill in start
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(mailserver)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(mailserver)
 # Fill in end
 # recv = clientSocket.recv(1024).decode()
 
 
 # Send HELO command and print server response.
-heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand.encode())
-recv1 = clientSocket.recv(1024).decode()
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
 
 # Send MAIL FROM command and print server response.
 # Fill in start
-fromCommand = 'MAIL FROM:Sudha' + emailFrom + '\r\n'
-clientSocket.send(fromCommand.encode())
-recv2 = clientSocket.recv(1024).decode()
+    fromCommand = 'MAIL FROM:Sudha' + emailFrom + '\r\n'
+    clientSocket.send(fromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
 
 # Fill in end
 
 # Send RCPT TO command and print server response.
 # Fill in start
-rcptCommand = 'RCPT TO: ' + emailTo + '\r\n'
-clientSocket.send(rcptCommand.encode())
-recv3 = clientSocket.recv(1024).decode()
+    rcptCommand = 'RCPT TO: ' + emailTo + '\r\n'
+    clientSocket.send(rcptCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
 
 # Fill in end
 
 # Send DATA command and print server response.
 # Fill in start
-dataCommand = 'DATA\r\n'
-clientSocket.send(dataCommand.encode())
-recv4 = clientSocket.recv(1024).decode()
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
 
 # Fill in end
 
 # Send message data.
 # Fill in start
-msg = "\r\n I love computer networks!"
-clientSocket.send(msg.encode())
+    msg = "\r\n I love computer networks!"
+    clientSocket.send(msg.encode())
 # Fill in end
 
 # Message ends with a single period.
 # Fill in start
-endmsg = "\r\n.\r\n"
-clientSocket.send(endmsg.encode())
-recv5 = clientSocket.recv(1024).decode()
+    endmsg = "\r\n.\r\n"
+    clientSocket.send(endmsg.encode())
+    recv5 = clientSocket.recv(1024).decode()
 
 # Fill in end
 
 # Send QUIT command and get server response.
 # Fill in start
-quitCommand = 'QUIT\r\n'
-clientSocket.send(quitCommand.encode())
-recv6 = clientSocket.recv(1024).decode()
+    quitCommand = 'QUIT\r\n'
+    clientSocket.send(quitCommand.encode())
+    recv6 = clientSocket.recv(1024).decode()
 
 # Fill in end
 if __name__ == '__main__':
